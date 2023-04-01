bc3aaaa50a314dc3e2c2acbac92156914b4c7108
Marlon S. Machado <103858936+MarlonDevLP@users.noreply.github.com>
2023-04-01 16:17:23 -0300

    Update bank_controller_exceptions.dart

--- Tratando_Exceptions_dart/exceptions/bank_controller_exceptions.dart
+++ Tratando_Exceptions_dart/exceptions/bank_controller_exceptions.dart
@@ -3,8 +3,23 @@ class SenderIdInvalidExceptions implements Exception {
   SenderIdInvalidExceptions({required this.idSender});
 }
 
-class ReceiverIdInvalidExceptions implements Exception {}
+class ReceiverIdInvalidExceptions implements Exception {
+  String idReceiver;
+  ReceiverIdInvalidExceptions({required this.idReceiver});
+}
 
-class SenderNotAuthenticatedException implements Exception {}
+class SenderNotAuthenticatedException implements Exception {
+  String idSender;
+  SenderNotAuthenticatedException({required this.idSender});
+}
 
-class SenderBalanceLowerThanAmoutException implements Exception {}
+class SenderBalanceLowerThanAmoutException implements Exception {
+  String idSender;
+  double senderBalance;
+  double amount;
+
+  SenderBalanceLowerThanAmoutException(
+      {required this.idSender,
+      required this.amount,
+      required this.senderBalance});
+}
