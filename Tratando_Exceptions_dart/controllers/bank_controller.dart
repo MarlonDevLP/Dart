0f76947f4e6327552bde9d90f94f8b91881fe8af
Marlon S. Machado <103858936+MarlonDevLP@users.noreply.github.com>
2023-03-31 22:29:23 -0300

    controllers

---
+++ Tratando_Exceptions_dart/controllers/bank_controller.dart
@@ -0,0 +1,49 @@
+import '../exceptions/bank_controller_exceptions.dart';
+import '../models/account.dart';
+
+class BankController {
+  final Map<String, Account> _database = {};
+
+  addAccount({required String id, required Account account}) {
+    _database[id] = account;
+  }
+
+  bool makeTransfer(
+      {required String idSender,
+      required String idReceiver,
+      required double amount}) {
+    // Verificar se ID de remetente é válido
+    if (!verifyId(idSender)) {
+      throw SenderIdInvalidExceptions(idSender: idSender);
+
+    }
+
+    // Verificar se ID de destinatário é válido
+    if (!verifyId(idReceiver)) {
+      throw ReceiverIdInvalidExceptions();
+    }
+
+    Account accountSender = _database[idSender]!;
+    Account accountReceiver = _database[idReceiver]!;
+
+    // Verificar se o remetente está autenticado
+    if (!accountSender.isAuthenticated) {
+      throw SenderNotAuthenticatedException();
+    }
+
+    // Verificar se o remetente possui saldo suficiente
+    if (accountSender.balance < amount) {
+      throw SenderBalanceLowerThanAmoutException();
+    }
+
+    // Se tudo estiver certo, efetivar transação
+    accountSender.balance -= amount;
+    accountReceiver.balance += amount;
+
+    return true;
+  }
+
+  bool verifyId(String id) {
+    return _database.containsKey(id);
+  }
+}
