f7d6d852f2d0c8d00b914e7bc803b849f1670c40
Marlon S. Machado <103858936+MarlonDevLP@users.noreply.github.com>
2023-04-02 11:59:57 -0300

    Update account.dart

--- Tratando_Exceptions_dart/models/account.dart
+++ Tratando_Exceptions_dart/models/account.dart
@@ -1,14 +1,17 @@
 class Account {
- String name;
- double balance;
- bool isAuthenticated;
+  String name;
+  double balance;
+  bool isAuthenticated;
+  DateTime? createdAt;
 
- Account(
-     {required this.name,
-     required this.balance,
-     required this.isAuthenticated});
+  Account(
+      {required this.name,
+      required this.balance,
+      required this.isAuthenticated, this.createdAt}); //:
+  //assert(name.isNotEmpty, "O nome na pode ser uma String vazia."), Testes em tempo de desenvolvimento
+  //assert(balance >= 0, "O Saldo não pode ser negativo");
 
- editBalance({required value}) {
-   balance = balance + value;
- }
+  editBalance({required value}) {
+    balance = balance + value;
+  }
 }
